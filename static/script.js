d183c5b48562ec025f90cf79d2a575f0e796485b
Akash <kedia23akash@gmail.com>
2021-04-04 15:39:56 -0700

    Added pdf file uploads

--- static/script.js
+++ static/script.js
@@ -1,26 +1,14 @@
-/**
- * Copyright 2018, Google LLC
- * Licensed under the Apache License, Version 2.0 (the `License`);
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an `AS IS` BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
- 
-// [START gae_python38_log]
-// [START gae_python3_log]
-'use strict';
+var fileUploader1 = document.getElementById("fileUploader1");
+var fileUploader2 = document.getElementById("fileUploader2");
+fileUploader.addEventListener("change", handleFileUpload, false);
 
-window.addEventListener('load', function () {
+function handleFileUpload(event) {
+  var file = event.target.files[0];
+  var reader = new FileReader();
 
-  console.log("Hello World!");
+  reader.onload = function (e) {
+    localStorage.setItem("content", e.target.result);
+  };
 
-});
-// [END gae_python3_log]
-// [END gae_python38_log]
+  reader.readAsText(file, "base64");
+}
