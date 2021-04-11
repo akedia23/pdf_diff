3e52eb136945adff9a7a94785e2af46971516ca4
Akash <kedia23akash@gmail.com>
2021-04-11 14:38:13 -0700

    Renders pdfs side-by-side on separate route

--- main.py
+++ main.py
@@ -14,33 +14,67 @@
 
 # [START gae_python38_render_template]
 # [START gae_python3_render_template]
-import datetime
+import os
 
-from flask import Flask, render_template
+from flask import (
+    Flask,
+    request,
+    redirect,
+    url_for,
+    render_template,
+    send_from_directory,
+)
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tmp")
+# UPLOAD_FOLDER = "/tmp/"
+ALLOWED_EXTENSIONS = {"pdf"}
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
-@app.route('/')
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/", methods=["GET", "POST"])
 def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
-                   datetime.datetime(2018, 1, 2, 10, 30, 0),
-                   datetime.datetime(2018, 1, 3, 11, 0, 0),
-                   ]
+    if request.method == "POST":
+        if "file1" not in request.files:
+            print("No left file attached in request")
+            return redirect(request.url)
+        if "file2" not in request.files:
+            print("No right file attached in request")
+            return redirect(request.url)
+        file1 = request.files["file1"]
+        file2 = request.files["file2"]
+        if file1.filename == "":
+            print("No left file selected")
+            return redirect(request.url)
+        if file2.filename == "":
+            print("No right file selected")
+            return redirect(request.url)
+        if (file1 and allowed_file(file1.filename)) and (
+            file2 and allowed_file(file2.filename)
+        ):
+            filename1 = secure_filename(file1.filename)
+            filename2 = secure_filename(file2.filename)
+            file_path1 = os.path.join(app.config["UPLOAD_FOLDER"], filename1)
+            file1.save(file_path1)
+            file_path2 = os.path.join(app.config["UPLOAD_FOLDER"], filename2)
+            file2.save(file_path2)
+            # process_file(os.path.join(app.config["UPLOAD_FOLDER"], filename), filename)
+            return redirect(url_for("uploaded_file"))
+    return render_template("index.html")
 
-    return render_template('index.html', times=dummy_times)
+
+@app.route("/diff")
+def uploaded_file():
+
+    return render_template("diff.html")
+    # return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 
-if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_python3_render_template]
-# [END gae_python38_render_template]
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
